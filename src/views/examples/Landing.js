ad2839b41373fe37382b01da33ed9f91fba04c3b
udev <udev@golivemo.com>
2023-09-16 16:21:00 -0700

    demo 4

--- src/views/examples/Landing.js
+++ src/views/examples/Landing.js
@@ -38,10 +38,6 @@ import {
 
 // core components
 import DemoNavbar from 'components/Navbars/DemoNavbar.js';
-import CardsFooter from 'components/Footers/CardsFooter.js';
-
-// index page sections
-import Download from '../IndexSections/Download.js';
 
 class Landing extends React.Component {
   state = {};
